6d47faed779b9f9604756764ed7c7f5455c12b3c
Fortune Ekeruo <legalmody@gmail.com>
2018-06-16 17:04:34 +0100

    Add a registration with antd components

--- packages/web/src/modules/register/views/RegisterView.tsx
+++ packages/web/src/modules/register/views/RegisterView.tsx
@@ -1,8 +1,50 @@
-import * as React from 'react';
-import Button from 'antd/lib/button';
+import * as React from "react";
+import { Form, Icon, Input, Button } from "antd";
+const FormItem = Form.Item;
 
 export class RegisterView extends React.PureComponent {
-    render() {
-        return (<Button type='primary'>Button</ Button>);
-    }
+  render() {
+    return (
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: 400, margin: 'auto' }}>
+            <h1>Register</h1>
+            <FormItem>
+            <Input
+                prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Username"
+            />
+            </FormItem>
+            <FormItem>
+            <Input
+                prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+                type="password"
+                placeholder="Password"
+            />
+            </FormItem>
+            <FormItem>
+            <Input
+                prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+                type="confirmpassword"
+                placeholder="Confirm Password"
+            />
+            </FormItem>
+            <FormItem>
+            <a className="login-form-forgot" href="">
+                Forgot password
+            </a>
+            </FormItem>
+            <FormItem>
+            <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+            >
+                Register 
+            </Button>
+                Or <a href="">login now!</a>
+            </FormItem>
+        </div>
+      </div>
+    );
+  }
 }

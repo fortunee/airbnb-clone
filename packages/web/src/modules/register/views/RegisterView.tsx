cd5b18f7667f8a5eb6bead6831c5fae297f62d2d
Fortune Ekeruo <legalmody@gmail.com>
2018-06-16 16:46:44 +0100

    Add a sample antd button and a root index.css file

--- packages/web/src/modules/register/views/RegisterView.tsx
+++ packages/web/src/modules/register/views/RegisterView.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import Button from 'antd/lib/button';
 
 export class RegisterView extends React.PureComponent {
     render() {
-        return <div>Welcome to Register Page</div>;
+        return (<Button type='primary'>Button</ Button>);
     }
 }

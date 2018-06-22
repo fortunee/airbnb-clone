7540f584a078a6d01bb2f8884a16b07f0e87e8bc
Fortune Ekeruo <legalmody@gmail.com>
2018-06-22 20:09:15 +0100

    Add mutation query and pass to graphql to return a mutated component

--- packages/controller/src/modules/RegisterController/index.tsx
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import { graphql, ChildMutateProps } from 'react-apollo';
+import gql from 'graphql-tag';
 
 interface Props {
     children: (
         data: { submit: (values: any) => Promise<null> }
     ) => JSX.Element | null
 }
-export class RegisterController extends React.PureComponent<Props> {
+class Register extends React.PureComponent<ChildMutateProps<Props, any, any>> {
     async submit (values: any) {
         console.log(values);
+        this.props.mutate({
+            variables: values
+        });
         return null;
     }
 
@@ -15,3 +20,15 @@ export class RegisterController extends React.PureComponent<Props> {
         return this.props.children({ submit: this.submit });
     }
 };
+
+const registerMutation = gql`
+    mutation($email: String!, $password: String!) {
+        register(email: $email, password:  $password) {
+        path,
+        message
+        }
+    }
+`;
+
+export const RegisterController = graphql(registerMutation)(Register);
+

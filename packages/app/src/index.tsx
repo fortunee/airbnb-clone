45195bbcec1daea2cc20524aa5c9201d9c012b1a
Fortune Ekeruo <legalmody@gmail.com>
2018-07-03 13:56:49 +0100

    Add routes and register connector for react-native

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ApolloProvider } from "react-apollo";
 import { client } from './apollo';
+import { Routes } from './routes';
 
 export default class App extends React.PureComponent {
     render() {

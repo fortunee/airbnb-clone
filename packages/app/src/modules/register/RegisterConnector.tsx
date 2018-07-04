1efd965332cc6e5ecbb6bd0485fd12e5ad569d0e
Fortune Ekeruo <legalmody@gmail.com>
2018-07-04 16:06:53 +0100

    Renamve view to views in the RN app

--- packages/app/src/modules/register/RegisterConnector.tsx
+++ packages/app/src/modules/register/RegisterConnector.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { RegisterView } from './views/RegisterView';
+import { RegisterView } from './view/RegisterView';
 import { RegisterController } from '@abb/controller';
 
 export class RegisterConnector extends React.PureComponent {

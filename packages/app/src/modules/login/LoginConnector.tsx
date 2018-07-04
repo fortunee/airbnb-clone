1999e2c36032bc18382b950f0e441f22a77f40ef
Fortune Ekeruo <legalmody@gmail.com>
2018-07-04 19:12:24 +0100

    Hookup login controller and comment sendEmail verification in the server

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-// import { RegisterController } from '@abb/controller';
 import { LoginView } from './view/LoginView';
+import { LoginController } from '@abb/controller';
 
 export class LoginConnector extends React.PureComponent {
-    onSubmit = async (values: any) => {
-        console.log(values);
-
-        return null;
-    }
-
     render() {
         return (
-            <LoginView submit={this.onSubmit} />
+            <LoginController>
+                {({ submit }) => <LoginView submit={submit} />}
+            </LoginController>
         )
     }
-}
+};

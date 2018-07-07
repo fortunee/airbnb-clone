8aeb3a65d8058db45e78299f7710b0ca831cb23b
Fortune Ekeruo <legalmody@gmail.com>
2018-07-07 21:47:22 +0100

    Move SID_KEY to a constant.ts file

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { LoginView } from './view/LoginView';
 import { LoginController } from '@abb/controller';
 import { SecureStore } from 'expo';
+import { SID_KEY } from './../shared/constant';
 
 export class LoginConnector extends React.PureComponent {
     saveSessionId = (sid: string) => {
-        SecureStore.setItemAsync('sid', sid);
+        SecureStore.setItemAsync(SID_KEY, sid);
     }
 
     render() {

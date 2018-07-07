2f2112e0ed97a787281b37f42a81202e65ad16ac
Fortune Ekeruo <legalmody@gmail.com>
2018-07-07 21:43:59 +0100

    save sid to secure store

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { LoginView } from './view/LoginView';
 import { LoginController } from '@abb/controller';
+import { SecureStore } from 'expo';
 
 export class LoginConnector extends React.PureComponent {
+    saveSessionId = (sid: string) => {
+        SecureStore.setItemAsync('sid', sid);
+    }
+
     render() {
         return (
-            <LoginController>
+            <LoginController onSessionId={this.saveSessionId}>
                 {({ submit }) => <LoginView submit={submit} />}
             </LoginController>
         )

9199abb19d4311f9760333e836e130d1e3794d7a
Fortune Ekeruo <legalmody@gmail.com>
2018-07-09 22:52:11 +0100

    Use invalid login instead of invalid email on login yup schema

--- packages/common/src/yupSchemas/user.ts
+++ packages/common/src/yupSchemas/user.ts
@@ -34,7 +34,7 @@ export const loginSchema = yup.object().shape({
     email: yup
         .string()
         .min(3, invalidLogin)
-        .max(255, invalidEmail)
+        .max(255, invalidLogin)
         .email(invalidLogin)
         .required(fieldRequired),
     password: yup

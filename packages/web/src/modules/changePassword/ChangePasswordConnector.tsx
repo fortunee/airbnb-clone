e66d1f5b37c506ec848b74d33e48d073558e357c
Fortune Ekeruo <legalmody@gmail.com>
2018-07-15 22:33:48 +0100

    Grab the url param and set type of key with RouterComponentProps from react-router-dom

--- packages/web/src/modules/changePassword/ChangePasswordConnector.tsx
+++ packages/web/src/modules/changePassword/ChangePasswordConnector.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import { ChangePasswordView } from './view/ChangePasswordView';
 
 
-export class ChangePasswordConnector extends React.PureComponent {
+export class ChangePasswordConnector extends React.PureComponent<RouteComponentProps<{ key: string }>> {
     submit = async (values: any) => {
         console.log(values);
         return null;
     }
 
     render() {
+        const { match: { params: { key } } } = this.props;
         return (
             <ChangePasswordView submit={this.submit} />
         );

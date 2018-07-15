8c163110d6dcfc76696c7fa888baf422875e2689
Fortune Ekeruo <legalmody@gmail.com>
2018-07-15 22:29:25 +0100

    Add a route for change password

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { RegisterConnector } from '../modules/register/RegisterConnector';
 import { LoginConnector } from '../modules/login/LoginConnector';
 import { ForgotPasswordConnector } from '../modules/ForgotPassword/ForgotPasswordConnector';
+import { ChangePasswordConnector } from '../modules/changePassword/ChangePasswordConnector';
 
 export const Routes = () => (
     <BrowserRouter>
@@ -10,6 +11,8 @@ export const Routes = () => (
             <Route exact={true} path='/register' component={RegisterConnector} />
             <Route exact={true} path='/login' component={LoginConnector} />
             <Route exact={true} path='/forgot-password' component={ForgotPasswordConnector} />
+            <Route exact={true} path='/change-password:key' component={ChangePasswordConnector} />
         </Switch>
     </BrowserRouter>
 );
+ 

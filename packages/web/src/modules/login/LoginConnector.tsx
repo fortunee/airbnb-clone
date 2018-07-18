a250a438019d7f1e24b1d11f9088abcaa8ad5b85
Fortune Ekeruo <legalmody@gmail.com>
2018-07-18 13:47:15 +0100

    Add onFinish to login

--- packages/web/src/modules/login/LoginConnector.tsx
+++ packages/web/src/modules/login/LoginConnector.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { LoginView } from './view/LoginView';
 import { LoginController } from '@abb/controller';
+import { RouteComponentProps } from 'react-router-dom';
 
-export class LoginConnector extends React.PureComponent {
+export class LoginConnector extends React.PureComponent<RouteComponentProps<{}>> {
+    onFinish = () => {
+        this.props.history.push('/')
+    }
     render() {
         return (
             <LoginController>
-            {({ submit }) => <LoginView submit={submit} />}
+            {({ submit }) => <LoginView onFinish={this.onFinish} submit={submit} />}
             </LoginController>
         );
     }

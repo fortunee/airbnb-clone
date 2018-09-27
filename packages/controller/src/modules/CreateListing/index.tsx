393c9232a83daf72a94648e0fb9dfed6a5de117b
Fortune Ekeruo <legalmody@gmail.com>
2018-09-27 23:06:12 +0100

    add a create listing graphql query and a HOC

---
+++ packages/controller/src/modules/CreateListing/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { CreateListingMutation, CreateListingMutationVariables } from '../../schemaTypes';
+
+const createListingMutation = gql`
+    mutation CreateListingMutation(
+        $name: String!
+        $category: String!
+        $description: String!
+        $price: Int!
+        $beds: Int!
+        $guests: Int!
+        $latitude: Float!
+        $longitude: Float!
+        $amenities: [String!]!
+    ) {
+        createListing(
+            input: {
+                name: $name
+                category: $category
+                description: $description
+                price: $price
+                beds: $beds
+                guests: $guests
+                latitude: $latitude
+                longitude: $longitude
+                amenities: $amenities
+            }
+        )
+    }
+`;
+
+export interface NewPropsCreateListing {
+    createListing: (variables: CreateListingMutationVariables) => void;
+}
+
+export const withCreateListing = graphql<
+    any,
+    CreateListingMutation,
+    CreateListingMutationVariables,
+    NewPropsCreateListing
+>(createListingMutation, {
+    props: ({ mutate }) => ({
+        createListing: (variables) => {
+            if (!mutate) {
+                return;
+            }
+
+            mutate({
+                variables
+            })
+        }
+    })
+});
--- packages/controller/src/schemaTypes.ts
+++ packages/controller/src/schemaTypes.ts
@@ -26,6 +26,30 @@ export interface ForgotPasswordChangeMutationVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: CreateListingMutation
+// ====================================================
+
+export interface CreateListingMutation {
+  createListing: boolean;
+}
+
+export interface CreateListingMutationVariables {
+  name: string;
+  category: string;
+  description: string;
+  price: number;
+  beds: number;
+  guests: number;
+  latitude: number;
+  longitude: number;
+  amenities: string[];
+}
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: SendForgotPasswordMutation
 // ====================================================
 

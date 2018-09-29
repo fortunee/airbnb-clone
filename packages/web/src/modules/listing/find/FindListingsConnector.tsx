e05767c3cb35659b18061f65dd0ca191b9c9f107
Fortune Ekeruo <legalmody@gmail.com>
2018-09-29 22:08:22 +0100

    add a route to display the listings

---
+++ packages/web/src/modules/listing/find/FindListingsConnector.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class FindListingsComponent extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export const FindListingsConnector = FindListingsComponent;
--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -8,6 +8,7 @@ import { ForgotPasswordConnector } from '../modules/forgotPassword/ForgotPasswor
 import { ChangePasswordConnector } from '../modules/changePassword/ChangePasswordConnector';
 import { GenericMesssageComponent } from '../modules/GenericMessage';
 import { CreateListingConnector } from '../modules/listing/create/CreateListingConnector';
+import { FindListingsConnector } from '../modules/listing/find/FindListingsConnector';
 
 export const Routes = () => (
     <BrowserRouter>
@@ -17,6 +18,7 @@ export const Routes = () => (
             <Route exact={true} path='/forgot-password' component={ForgotPasswordConnector} />
             <Route exact={true} path='/change-password/:key' component={ChangePasswordConnector} />
             <Route path='/m' component={GenericMesssageComponent} />
+            <Route path='/listing' component={FindListingsConnector} />
             <AuthRoute path='/create-listing' component={CreateListingConnector} />
         </Switch>
     </BrowserRouter>

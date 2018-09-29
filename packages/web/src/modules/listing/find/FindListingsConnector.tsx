d943cc93f96b282ac41d27313e482ba8731a320a
Fortune Ekeruo <legalmody@gmail.com>
2018-09-29 22:23:52 +0100

    Display loading and map through listing and display in antd card

--- packages/web/src/modules/listing/find/FindListingsConnector.tsx
+++ packages/web/src/modules/listing/find/FindListingsConnector.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
+import { Card } from 'antd';
+import { withFindListings, WithFindListings } from '@abb/controller';
 
-class FindListingsComponent extends React.PureComponent {
+const { Meta } = Card;
+
+class FindListingsComponent extends React.PureComponent<WithFindListings> {
     render() {
+        const { listings, loading } = this.props;
+
         return (
             <div>
-                Hello
+                {loading && <div>...loading</div>}
+                {listings.map(listing => 
+                    (<Card
+                            key={`${listing.id}-card`}
+                            hoverable={true}
+                            style={{ width: 240 }}
+                            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta
+                                title={listing.name}
+                                description='www.instagram.com'
+                        />
+                    </Card>))}
             </div>
         );
     }
 }
 
-export const FindListingsConnector = FindListingsComponent;
+export const FindListingsConnector = withFindListings(FindListingsComponent);

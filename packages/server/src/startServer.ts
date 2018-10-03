d39c3a5c02095a2e381e48bb754e3d4987af4677
Fortune Ekeruo <legalmody@gmail.com>
2018-10-03 22:32:25 +0100

    pass batchUsers func to dataloader instance of the userLoader

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -15,6 +15,7 @@ import { confirmEmail } from "./routes/confirmEmail";
 import { genSchema } from "./utils/genSchema";
 import { redisSessionPrefix } from "./constants";
 import { createTestConn } from "./testUtils/createTestConn";
+import { userLoader } from "./loaders/UserLoader";
 
 const SESSION_SECRET = "ajslkjalksjdfkl";
 const RedisStore = connectRedis(session as any);
@@ -33,7 +34,8 @@ export const startServer = async () => {
       redis,
       url: request.protocol + "://" + request.get("host"),
       session: request.session,
-      req: request
+      req: request,
+      userLoader: userLoader()
     })
   });
 

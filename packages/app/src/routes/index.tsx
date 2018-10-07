5dfc4c05abbc9baf0e5bd5f657c2ddb3f4f2e475
Fortune Ekeruo <legalmody@gmail.com>
2018-10-07 22:53:55 +0100

    Use cookies for authentication in the backend and get rid of secure store session

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { NativeRouter, Route, Switch } from 'react-router-native';
 import { RegisterConnector } from '../modules/register/RegisterConnector';
 import { LoginConnector } from '../modules/login/LoginConnector';
+import { Me } from '../modules/me/Me';
 
 export const Routes = () => (
-    <NativeRouter initialEntries={['/login']}>
+    <NativeRouter initialEntries={['/me']}>
         <Switch>
             <Route exact={true} path='/register' component={RegisterConnector} />
             <Route exact={true} path='/login' component={LoginConnector} />
+            <Route exact={true} path='/me' component={Me} />
         </Switch>
     </NativeRouter>
 );

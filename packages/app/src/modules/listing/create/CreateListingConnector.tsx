c31e7f7dc143f0f95d18f49008fd2141126c0901
Fortune Ekeruo <legalmody@gmail.com>
2018-10-09 20:46:19 +0100

    Add all specific fields for creating a listing in the react native app

--- packages/app/src/modules/listing/create/CreateListingConnector.tsx
+++ packages/app/src/modules/listing/create/CreateListingConnector.tsx
@@ -1,53 +1,125 @@
-import * as React from 'react'; 
-import { Formik } from 'formik';
-import { withCreateListing, WithCreateListing } from '@abb/controller';
-import { RouteComponentProps } from 'react-router-native';
-import { Text } from 'react-native';
+import * as React from "react";
+import { Formik, Field } from "formik";
+import { RouteComponentProps } from "react-router-native";
+import { Text, View, ScrollView } from "react-native";
+import { Button } from "react-native-elements";
+
+import { withCreateListing, WithCreateListing } from "@abb/controller";
+import { InputField } from "../../shared/inputField";
 
 interface FormValues {
-    name: string;
-    picture: null;
-    category: string;
-    description: string;
-    price: string;
-    beds: string;
-    guests: string;
-    latitude: string;
-    longitude: string;
-    amenities: string[];
+  name: string;
+  picture: null;
+  category: string;
+  description: string;
+  price: string;
+  beds: string;
+  guests: string;
+  latitude: string;
+  longitude: string;
+  amenities: string[];
 }
 
-export class CreateListingComponent extends React.PureComponent<RouteComponentProps<{}> & WithCreateListing> {
+export class CreateListingComponent extends React.PureComponent<
+  RouteComponentProps<{}> & WithCreateListing
+> {
+  submit = async (
+    values: FormValues /*{ setSubmitting }: FormikActions<FormValues>*/
+  ) => {
+    console.log(values);
+    // await this.props.createListing(values);
+    // setSubmitting(false);
+  };
 
+  render() {
+    return (
+      <Formik<{}, FormValues>
+        initialValues={{
+          name: "",
+          picture: null,
+          category: "",
+          description: "",
+          price: "0",
+          beds: "0",
+          guests: "0",
+          latitude: "0",
+          longitude: "0",
+          amenities: []
+        }}
+        onSubmit={this.submit}
+      >
+        {({ handleSubmit}) => (
+          <View style={{ flex: 1, display: "flex", justifyContent: "center" }}>
+            <ScrollView style={{ marginTop: 20, padding: 20 }}>
+              <Text style={{ fontSize: 30, marginBottom: 10 }}>
+                Create listing
+              </Text>
+              <Field
+                label="Name"
+                name="name"
+                placeholder="Name"
+                component={InputField}
+              />
 
-    submit = async (values: FormValues /*{ setSubmitting }: FormikActions<FormValues>*/) => {
-        console.log(values);
-        // await this.props.createListing(values);
-        // setSubmitting(false);
-    }
+              <Field
+                label="Category"
+                name="category"
+                placeholder="Category"
+                component={InputField}
+              />
 
-    render() {
-        return (
-            <Formik<{}, FormValues> initialValues={{
-                name: '',
-                picture: null,
-                category: '',
-                description: '',
-                price: '0',
-                beds: '0',
-                guests: '0',
-                latitude: '0',
-                longitude: '0',
-                amenities: []
-            }} onSubmit={this.submit}>
-                {
-                    ({ isSubmitting, values }) => (
-                        <Text>Hello Listing form</Text>
-                    )
-                }
-            </Formik>
-        );
-    }
+              <Field
+                label="Description"
+                name="description"
+                placeholder="Description"
+                component={InputField}
+              />
+
+              <Field
+                label="Price"
+                name="price"
+                placeholder="Price"
+                component={InputField}
+                useNumberInput={true}
+              />
+
+              <Field
+                label="Beds"
+                name="beds"
+                placeholder="Beds"
+                component={InputField}
+                useNumberInput={true}
+              />
+
+              <Field
+                label="Guests"
+                name="guests"
+                placeholder="Guests"
+                component={InputField}
+                useNumberInput={true}
+              />
+
+              <Field
+                label="Longitude"
+                name="longitude"
+                placeholder="Longitude"
+                component={InputField}
+                useNumberInput={true}
+              />
+              <Field
+                label="Latitude"
+                name="latitude"
+                placeholder="Latitude"
+                component={InputField}
+                useNumberInput={true}
+              />
+              <Button onPress={handleSubmit} title="Create" />
+            </ScrollView>
+          </View>
+        )}
+      </Formik>
+    );
+  }
 }
 
 export const CreateListingConnector = withCreateListing(CreateListingComponent);

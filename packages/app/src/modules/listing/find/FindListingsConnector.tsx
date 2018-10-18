d8dd0ef387b26e554bcb72eccc82cdac5ec08bb0
Fortune Ekeruo <legalmody@gmail.com>
2018-10-18 21:28:50 +0100

    Display each listing in a card

--- packages/app/src/modules/listing/find/FindListingsConnector.tsx
+++ packages/app/src/modules/listing/find/FindListingsConnector.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
+import { withFindListings, WithFindListings } from '@abb/controller';
+import { Card } from 'react-native-elements';
 
-export class FindListingsConnector extends React.PureComponent {
+class FindListingComponent extends React.PureComponent<WithFindListings> {
     render() {
+        const { listings } = this.props;
+
         return (
-            <Text>Hi Listings Display</Text>
+            <ScrollView style={{ marginTop: 20 }}>
+                {
+                    listings.map((listing) => (
+                        <Card
+                            key={`${listing.id}-listing`}
+                            title={listing.name}
+                            image={listing.pictureUrl ? { uri: listing.pictureUrl } : undefined }
+                        >
+                            <Text>Owner: {listing.owner.email}</Text>
+                        </Card>
+                    ))
+                }
+            </ScrollView>
         );
     }
 }
+
+export const FindListingsConnector = withFindListings(FindListingComponent);

6eba7ec04496049204856e91481bf872528929dd
Fortune Ekeruo <legalmody@gmail.com>
2018-10-18 21:00:59 +0100

    Add a RN findListingConnector

---
+++ packages/app/src/modules/listing/find/FindListingsConnector.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export class FindListingsConnector extends React.PureComponent {
+    render() {
+        return (
+            <Text>Hi Listings Display</Text>
+        );
+    }
+}

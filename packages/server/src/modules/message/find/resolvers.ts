a38ddb17a538bfdb66228622a62cb23fecf703dc
Fortune Ekeruo <legalmody@gmail.com>
2018-10-19 22:55:32 +0100

    Use the userLoader to fetch the user data given the userId

--- packages/server/src/modules/message/find/resolvers.ts
+++ packages/server/src/modules/message/find/resolvers.ts
@@ -2,6 +2,10 @@ import { ResolverMap } from "../../../types/graphql-utils";
 import { Message } from "../../../entity/Message";
 
 export const resolvers: ResolverMap = {
+    Message: {
+        user: ({ userId }, _, { userLoader }) => userLoader.load(userId)
+    },
+     
     Query: {
         messages: async (_, { listingId }, { session }) => {
             return Message.find({

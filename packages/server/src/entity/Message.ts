2ca9f5b22296e413228e589df83a40b77a365435
Fortune Ekeruo <legalmody@gmail.com>
2018-10-19 22:16:23 +0100

    Add a messaging entity with one to many relationships to user and listing entities

---
+++ packages/server/src/entity/Message.ts
@@ -0,0 +1,29 @@
+import {
+    Entity,
+    Column,
+    BaseEntity,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    // JoinColumn,
+  } from 'typeorm';
+  import { User } from './User';
+import { Listing } from './Listing';
+  
+  @Entity('messages')
+  export class Messaging extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid') id: string;
+  
+    @Column('text')
+    text: string;
+    
+    @Column("uuid") userId: string;
+  
+    @ManyToOne(() => User)
+    user: User;
+
+    @Column("uuid") listingId: string;
+  
+    @ManyToOne(() => Listing)
+    listing: Listing;
+  }
+  

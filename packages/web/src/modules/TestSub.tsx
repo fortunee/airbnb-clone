c3e0b8617a4e7e247dec96edfe8b1d4ec79aab63
Fortune Ekeruo <legalmody@gmail.com>
2018-12-02 22:32:37 +0100

    add a simple TestSub component for testing the pubsub

---
+++ packages/web/src/modules/TestSub.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Subscription } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const sub = gql`
+subscription {
+    newMessage (listingId: "617e2805-e3c1-4621-a53e-defc22833d7f") {
+      text
+      user {
+        id
+        email
+      }
+      listingId
+    }
+  }  
+`;
+export class TestSub extends React.PureComponent {
+    render() {
+        return (
+            <Subscription subscription={sub}>
+                {(data) => {
+                    console.log(data);
+                    return null
+                }}
+            </Subscription>
+        );
+    }
+}

dba5957d2140708427356bee5315a1061ad78682
Fortune Ekeruo <legalmody@gmail.com>
2019-08-11 19:49:30 +0100

    add a route for view chat messages and a message connector

---
+++ packages/web/src/modules/listing/messages/MessageConnector.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { ViewMessages } from '@abb/controller';
+
+export class MessageConnector extends React.PureComponent<
+  RouteComponentProps<{
+    listingId: string;
+  }>
+> {
+  render() {
+    const {
+      match: { params: { listingId } }
+    } = this.props;
+
+    return (
+      <ViewMessages listingId={listingId}>
+        {({ loading, messages }) => {
+          if (loading) {
+            return <div>loading...</div>
+          }
+          /**
+           * @todo: Dirty I know, :(
+           * Imma clean this up later, for now I'm just making sure this worrks
+           */
+          // tslint:disable-next-line: jsdoc-format
+          return <div>{messages.map((m, i) => <div key={`${i}-ml`}>{m.text}</div>)}</div>
+        }}
+      </ViewMessages>
+    )
+  }
+}

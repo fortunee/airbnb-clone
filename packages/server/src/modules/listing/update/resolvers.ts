6d5f013b78cac2531b80ca91964fc8570c1c7d4d
Fortune Ekeruo <legalmody@gmail.com>
2020-05-27 04:04:53 +0100

    implement caching on listing update

--- packages/server/src/modules/listing/update/resolvers.ts
+++ packages/server/src/modules/listing/update/resolvers.ts
@@ -1,21 +1,37 @@
 import { ResolverMap } from "../../../types/graphql-utils";
 import { Listing } from "../../../entity/Listing";
 import { processUpload } from "../shared/processUpload";
+import { getConnection } from "typeorm";
+import { listingCacheKey } from "../../../constants";
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    updateListing: async (_, { listingId, input: { picture, ...data } }) => {
+    updateListing: async (
+      _,
+      { listingId, input: { picture, ...data } },
+      { redis }
+    ) => {
       if (picture) {
         data.pictureUrl = await processUpload(picture);
       }
 
-      await Listing.update(
-        {
-          id: listingId,
-        },
-        {
-          ...data,
-        }
+      const {
+        raw: [updatedListing],
+      } = await getConnection()
+        .createQueryBuilder()
+        .update(Listing)
+        .set(data)
+        .where("id = :id", { id: listingId })
+        .returning("*")
+        .execute();
+
+      const redisIndex = (await redis.lrange(listingCacheKey, 0, -1)).findIndex(
+        (x: string) => JSON.parse(x).id === listingId
+      );
+      await redis.lset(
+        listingCacheKey,
+        redisIndex,
+        JSON.stringify(updatedListing)
       );
 
       return true;

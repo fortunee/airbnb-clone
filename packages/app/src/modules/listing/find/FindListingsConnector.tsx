0e4a733037d61e6a778e175f8acf98713eb53ea3
Fortune Ekeruo <legalmody@gmail.com>
2020-06-13 00:43:47 +0100

    implement search for the app

--- packages/app/src/modules/listing/find/FindListingsConnector.tsx
+++ packages/app/src/modules/listing/find/FindListingsConnector.tsx
@@ -1,28 +1,47 @@
-import * as React from 'react';
-import { Text, ScrollView } from 'react-native';
-import { withFindListings, WithFindListings } from '@abb/controller';
-import { Card } from 'react-native-elements';
+import * as React from "react";
+import { Text, ScrollView, TextInput, SafeAreaView } from "react-native";
+import { SearchListings } from "@abb/controller";
+import { Card } from "react-native-elements";
 
-class FindListingComponent extends React.PureComponent<WithFindListings> {
-    render() {
-        const { listings } = this.props;
-
-        return (
-            <ScrollView style={{ marginTop: 20 }}>
-                {
-                    listings.map((listing) => (
-                        <Card
-                            key={`${listing.id}-listing`}
-                            title={listing.name}
-                            image={listing.pictureUrl ? { uri: listing.pictureUrl } : undefined }
-                        >
-                            <Text>Owner: {listing.owner.email}</Text>
-                        </Card>
-                    ))
-                }
-            </ScrollView>
-        );
-    }
+interface State {
+  name: string;
 }
 
-export const FindListingsConnector = withFindListings(FindListingComponent);
+export class FindListingsConnector extends React.PureComponent<{}, State> {
+  state = {
+    name: "",
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <React.Fragment>
+        <SafeAreaView />
+        <TextInput
+          style={{ fontSize: 20, width: "100%" }}
+          placeholder="Search listing..."
+          onChangeText={(text) => this.setState({ name: text })}
+          value={name}
+        />
+        <SearchListings variables={{ input: { name }, limit: 5, offset: 0 }}>
+          {({ listings }) => (
+            <ScrollView style={{ marginTop: 20 }}>
+              {listings.map((listing) => (
+                <Card
+                  key={`${listing.id}-listing`}
+                  title={listing.name}
+                  image={
+                    listing.pictureUrl ? { uri: listing.pictureUrl } : undefined
+                  }
+                >
+                  <Text>Owner: {listing.owner.email}</Text>
+                </Card>
+              ))}
+            </ScrollView>
+          )}
+        </SearchListings>
+      </React.Fragment>
+    );
+  }
+}

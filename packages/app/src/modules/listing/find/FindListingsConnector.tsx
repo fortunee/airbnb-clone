af415ea89ccb81eb2903a6314d5046b546328be8
Fortune Ekeruo <legalmody@gmail.com>
2020-06-13 02:00:58 +0100

    add a slider for searching for number of guests and beds

--- packages/app/src/modules/listing/find/FindListingsConnector.tsx
+++ packages/app/src/modules/listing/find/FindListingsConnector.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
-import { Text, ScrollView, TextInput, SafeAreaView } from "react-native";
+import {
+  Text,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+  View,
+  Slider,
+} from "react-native";
 import { SearchListings } from "@abb/controller";
 import { Card } from "react-native-elements";
 
 interface State {
   name: string;
+  guests: number;
+  beds: number;
 }
 
 export class FindListingsConnector extends React.PureComponent<{}, State> {
   state = {
     name: "",
+    guests: 1,
+    beds: 1,
   };
 
   render() {
-    const { name } = this.state;
+    const { beds, guests, name } = this.state;
 
     return (
       <React.Fragment>
@@ -24,7 +35,25 @@ export class FindListingsConnector extends React.PureComponent<{}, State> {
           onChangeText={(text) => this.setState({ name: text })}
           value={name}
         />
-        <SearchListings variables={{ input: { name }, limit: 5, offset: 0 }}>
+        <View style={{ alignItems: "stretch", justifyContent: "center" }}>
+          <Slider
+            value={guests}
+            onValueChange={(value) => this.setState({ guests: value })}
+            step={1}
+            maximumValue={5}
+          />
+          <Text>Guests: {guests}</Text>
+        </View>
+        <View>
+          <Slider
+            value={beds}
+            onValueChange={(value) => this.setState({ beds: value })}
+          />
+          <Text>Beds: {beds}</Text>
+        </View>
+        <SearchListings
+          variables={{ input: { name, guests, beds }, limit: 5, offset: 0 }}
+        >
           {({ listings }) => (
             <ScrollView style={{ marginTop: 20 }}>
               {listings.map((listing) => (
